9f4518a78965b4328862b6806fabc0ffe18a05b5
Declan Valters <declan.valters@ed.ac.uk>
2017-12-06 19:15:18 +0000

    Environment vars file

---
+++ sources.sh
@@ -0,0 +1,17 @@
+export LVT_ARCH=linux_gfortran
+
+export LVT_CC=gcc
+export LVT_FC=gfortran
+# /usr/include/grib_api.h
+export LVT_GRIBAPI=/usr/share/grib_api
+export LVT_JASPER=
+export LVT_LIBESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/lib
+export LVT_MODESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/mod
+export LVT_SRC=/home/dvalters/dev/LVT_public_release_7.2r
+# /usr/include/
+export LVT_NETCDF=/usr/lib64/
+export LVT_HDF4=/usr/lib64/hdf/
+export LVT_HDF5=/usr/lib64/
+export LVT_HDFEOS=
+export LVT_GDAL=/usr/include/gdal/
+
