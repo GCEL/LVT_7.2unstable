7458db8c3051abf85a30568a7bd4abd280b1d5d2
Declan Valters <declan.valters@ed.ac.uk>
2017-12-08 17:00:37 +0000

    LD_LIBRARY_PATH source

--- sources.sh
+++ sources.sh
@@ -5,8 +5,8 @@ export LVT_FC=gfortran
 # /usr/include/grib_api.h
 export LVT_GRIBAPI=/usr/share/grib_api
 export LVT_JASPER=
-export LVT_LIBESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/lib
-export LVT_MODESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/mod
+export LVT_LIBESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/lib/libO/Linux.gfortran.64.openmpi.default
+export LVT_MODESMF=/home/dvalters/dev/esmf/DEFAULTINSTALLDIR/mod/modO/Linux.gfortran.64.openmpi.default
 export LVT_SRC=/home/dvalters/dev/LVT_public_release_7.2r
 # /usr/include/
 export LVT_NETCDF=/usr/lib64/
@@ -14,4 +14,5 @@ export LVT_HDF4=/usr/lib64/hdf/
 export LVT_HDF5=/usr/lib64/
 export LVT_HDFEOS=
 export LVT_GDAL=/usr/include/gdal/
-
+# LVT executable loads from the LD_LIRARY_PATH, so make sure this variable is exported
+export LD_LIBRARY_PATH=$LVT_HDF5:$LVT_LIBESMF:$LVT_NETCDF:$LVT_GRIB_API:$LD_LIBRARY_PATH
