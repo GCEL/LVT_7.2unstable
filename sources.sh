ec4d0268378ebf9c8671df8bf87f0843759ddadf
Declan Valters <declan.valters@ed.ac.uk>
2017-12-11 17:35:25 +0000

    recompile of ESMF

--- sources.sh
+++ sources.sh
@@ -5,8 +5,8 @@ export LVT_FC=gfortran
 # /usr/include/grib_api.h
 export LVT_GRIBAPI=/usr/share/grib_api
 export LVT_JASPER=
-export LVT_LIBESMF=/home/dvalters/LVT/esmf/DEFAULTINSTALLDIR/lib/libO/Linux.gfortran.64.openmpi.default
-export LVT_MODESMF=/home/dvalters/LVT/esmf/DEFAULTINSTALLDIR/mod/modO/Linux.gfortran.64.openmpi.default
+export LVT_LIBESMF=/home/dvalters/LVT/esmf52/lib/libO/Linux.gfortran.64.mpiuni.default/
+export LVT_MODESMF=/home/dvalters/LVT/esmf52/mod/modO/Linux.gfortran.64.mpiuni.default/
 export LVT_SRC=/home/dvalters/LVT/LVT_public_release_7.2r
 # /usr/include/
 export LVT_NETCDF=/usr/lib64/
